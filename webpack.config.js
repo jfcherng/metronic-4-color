2f769ce992bc8e32dce40e671ea426b9f7dfda40
Jack Cherng <jfcherng@gmail.com>
2018-11-10 02:28:08 +0800

    Remove webpack notification plugin
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -37,8 +37,7 @@ Encore
   // the public path used by the web server to access the previous directory
   .setPublicPath(publicPath)
   // .cleanupOutputBeforeBuild()
-  .enableBuildNotifications()
-  // .enableSourceMaps(!Encore.isProduction())
+  .enableSourceMaps(!Encore.isProduction())
   // uncomment to create hashed filenames (e.g. app.abc123.css)
   // .enableVersioning(Encore.isProduction())
   // uncomment if you use Sass/SCSS files
